3363bfd28aae5506a55376777338aca46db3a6f4
Eduardo Oliveira <eduardo_y05@outlook.com>
2023-04-01 13:52:02 -0300

    feat: begin setup (#3)

---
+++ packages/react/vite.config.ts
@@ -0,0 +1,31 @@
+import { defineConfig } from "vite";
+import path from "node:path";
+import react from "@vitejs/plugin-react";
+import dts from "vite-plugin-dts";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    dts({
+      insertTypesEntry: true,
+    }),
+  ],
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, "src/index.ts"),
+      name: "@inventare/react",
+      formats: ["es", "umd"],
+      fileName: (format) => `inventare.${format}.js`,
+    },
+    rollupOptions: {
+      external: ["react", "react-dom", "styled-components"],
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+        },
+      },
+    },
+  },
+});
