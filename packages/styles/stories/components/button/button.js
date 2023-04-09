4269f31dcbf74bf4f9fabdc8ef60659f39330924
Eduardo Oliveira <eduardo_y05@outlook.com>
2023-04-08 22:47:47 -0300

    feat: add square variant for buttons

--- packages/styles/stories/components/button/button.js
+++ packages/styles/stories/components/button/button.js
@@ -1,11 +1,12 @@
-export const createButton = ({ label, variant, disabled, onClick }) => {
+export const createButton = ({ label, variant, disabled, square, onClick }) => {
   const btn = document.createElement("button");
   btn.type = "button";
   btn.innerText = label;
   btn.addEventListener("click", onClick);
 
   variant = variant === "default" ? "" : variant;
-  btn.className = ["btn", variant].filter((item) => !!item).join(" ");
+  square = square ? "square" : "";
+  btn.className = ["btn", variant, square].filter((item) => !!item).join(" ");
 
   if (disabled) {
     btn.disabled = true;
