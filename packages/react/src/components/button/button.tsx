233164e57c4800f89381c1d64b4953409e6b373f
Eduardo Oliveira <eduardo_y05@outlook.com>
2023-04-15 14:26:54 -0300

    feat: add button

--- packages/react/src/components/button/button.tsx
+++ packages/react/src/components/button/button.tsx
@@ -4,5 +4,5 @@ import { ButtonProps } from "./button.types";
 export const Button = forwardRef<any, ButtonProps>((props, ref) => {
   const { as, children, className, ...rest } = props;
 
-  return createElement(as || "button", { ...rest, children });
+  return createElement(as || "button", { ...rest, ref, children });
 });
