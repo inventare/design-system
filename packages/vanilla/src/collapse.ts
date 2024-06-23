45f34bce56cfe93bf526dec780a79e9430ee6861
Eduardo Oliveira <eduardo_y05@outlook.com>
2024-06-23 10:59:10 -0300

    chore: add support for horizontal collapse

--- packages/vanilla/src/collapse.ts
+++ packages/vanilla/src/collapse.ts
@@ -6,11 +6,20 @@ interface CollapseObjects {
 }
 
 const CLASS_NAME_SHOW = 'show';
-const CLASS_NAME_COLLAPSE = 'collapse'
-const CLASS_NAME_COLLAPSING = 'collapsing'
-const CLASS_NAME_COLLAPSED = 'collapsed'
+const CLASS_NAME_COLLAPSE = 'collapse';
+const CLASS_NAME_COLLAPSING = 'collapsing';
+const CLASS_NAME_COLLAPSED = 'collapsed';
+const CLASS_NAME_HORIZONTAL = 'horizontal';
 
-const TRIGGER_SELECTOR = '[data-toggle="collapse"]'
+const TRIGGER_SELECTOR = '[data-toggle="collapse"]';
+
+const DIMENSION_VERTICAL = 'VERTICAL'
+const DIMENSION_HORIZONTAL = 'HORIZONTAL'
+type DIMENSION_TYPE = 'VERTICAL' | 'HORIZONTAL';
+const DIMENSIONS: Record<DIMENSION_TYPE, 'width' | 'height'> = {
+  'VERTICAL': 'height',
+  'HORIZONTAL': 'width',
+}
 
 function getCollapseObjects(target?: HTMLElement | null): CollapseObjects {
   if (!target) {
@@ -34,6 +43,30 @@ function getCollapseObjects(target?: HTMLElement | null): CollapseObjects {
   };
 }
 
+function capFirst(text: string) {
+  return text[0].toUpperCase() + text.substring(1).toLowerCase();
+}
+
+function getContentSize(content: HTMLElement, isOpen: boolean): {
+  value: string;
+  property: 'width' | 'height',
+} {
+  const dimension: DIMENSION_TYPE = content.classList.contains(CLASS_NAME_HORIZONTAL)
+    ? DIMENSION_HORIZONTAL
+    : DIMENSION_VERTICAL;
+  const property = `scroll${capFirst(DIMENSIONS[dimension])}`;
+  if (!isOpen) {
+    return {
+      value: '',
+      property: DIMENSIONS[dimension],
+    };
+  }
+  return {
+    value: `${(content as any)[property]}px`,
+    property: DIMENSIONS[dimension],
+  };
+}
+
 function clickOnCollapse(target?: HTMLElement | null) {
   const { toggle, content } = getCollapseObjects(target);
   if (!toggle || !content) {
@@ -49,18 +82,15 @@ function clickOnCollapse(target?: HTMLElement | null) {
     isOpen = Boolean(JSON.parse(isOpenAttr));
   }
 
-  let initialHeight: string = `${content.scrollHeight}px`;
-  if (!isOpen) {
-    initialHeight = '';
-  }
+  const { value, property } = getContentSize(content, isOpen);
 
-  content.style.height = initialHeight;
+  content.style[property] = value;
   content.offsetHeight; // reset animation
 
   content.classList.add(CLASS_NAME_COLLAPSING);
   content.classList.remove(CLASS_NAME_COLLAPSE, CLASS_NAME_SHOW)
-  toggle.setAttribute('aria-expanded', JSON.stringify(!isOpen));
   content.classList.toggle(CLASS_NAME_COLLAPSED, isOpen);
+  toggle.setAttribute('aria-expanded', JSON.stringify(!isOpen));
 
   const complete = () => {
     content.classList.remove(CLASS_NAME_COLLAPSING);
@@ -71,12 +101,9 @@ function clickOnCollapse(target?: HTMLElement | null) {
   };
   executeAfterTransition(complete, content);
 
-  let newHeight: string = '';
-  if (!isOpen) {
-    newHeight = `${content.scrollHeight}px`;
-  }
+  const { value: destValue } = getContentSize(content, !isOpen);
 
-  content.style.height = newHeight;
+  content.style[property] = destValue;
 }
 
 function handleDocumentBodyClick(ev: MouseEvent) {
