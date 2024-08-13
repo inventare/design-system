3e4cf39f5d1cc417205936ce5663934406c38a3f
Eduardo Oliveira <eduardo_y05@outlook.com>
2024-08-13 09:10:58 -0300

    feat: working on skeleton

---
+++ apps/docs/stories/components/Skeleton/Skeleton.ts
@@ -0,0 +1,26 @@
+export interface SkeletonProps {
+  borderRadius: 'none' | 'rounded' | 'medium' | 'large' | 'xlarge' | 'pill' | 'circle';
+  width: number;
+  height: number;
+}
+
+export const createSkeleton = ({
+  borderRadius,
+  width,
+  height,
+}: SkeletonProps) => {
+  const div = document.createElement('div');
+  div.style.width = width ? `${width}px` : '200px';
+  div.style.height = height ? `${height}px` : '200px';
+  div.className = 'skeleton';
+
+  if (borderRadius !== 'none') {
+    if (borderRadius === 'rounded') {
+      div.classList.add('rounded');
+    } else {
+      div.classList.add(`rounded-${borderRadius}`);
+    }
+  }
+
+  return div;
+};
