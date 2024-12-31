4142f0b872b5b760293cb626fb04d17b1b5d826d
Eduardo Oliveira <eduardo_y05@outlook.com>
2024-12-31 10:50:24 -0300

    feat: add as and forwardRef to button

--- packages/react/src/components/button/button.tsx
+++ packages/react/src/components/button/button.tsx
@@ -1,31 +1,38 @@
-import { ButtonHTMLAttributes } from 'react';
+import { createElement } from 'react';
 import clsx from 'clsx';
+import { forwardRef, HTMLProps } from '../../core';
 
 export type ButtonColor = 'primary' | 'danger';
 
 export type ButtonSize = 'small' | 'normal' | 'large';
 
-export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+export interface ButtonProps extends HTMLProps<"button"> {
   color?: ButtonColor;
   outline?: boolean;
   size?: ButtonSize;
 }
 
-export const Button = ({
-  color = 'primary',
-  outline = false,
-  size = 'normal',
-  className,
-  ...buttonProps
-}: ButtonProps) => (
-  <button
-    className={clsx({
-      [className || '']: true,
-      btn: true,
-      [color]: true,
-      [size]: true,
-      outline: outline,
-    })}
-    {...buttonProps}
-  />
-);
+export const Button = forwardRef<ButtonProps, "button">((props, ref) => {
+  const {
+    as,
+    color = 'primary',
+    outline = false,
+    size = 'normal',
+    className,
+    ...buttonProps
+  } = props;
+
+  return createElement(
+    as || 'button', {
+      className: clsx({
+        [className || '']: true,
+        btn: true,
+        [color]: true,
+        [size]: true,
+        outline: outline,
+      }),
+      ref,
+      ...buttonProps,
+    }
+  );
+});
